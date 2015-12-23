45e2f5075e927578cb118a8758817e82cc922594
miguel zuñiga <migzunig@alumnos.ubiobio.cl>
2015-12-23 05:32:34 -0300

    UP NAV

--- resources/views/admin/template/partial/nav.blade.php
+++ resources/views/admin/template/partial/nav.blade.php
@@ -1,19 +1,12 @@
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
 
-        <h3>TITULO</h3>
-        <ul class="nav navbar-nav navbar-right">
+        <h3>Proyecto Ingenieria de Softeware</h3>
 
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">GESTION DE GRUPOS<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="{{route('admin.usuario.index')}}">Mostrar Lista Usuarios</a></li>
-                    <li><a href="{{route('admin.usuario.create')}}">Registrar nuevo usuario</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Cerrar Sesión</a></li>
-                </ul>
-            </li>
-        </ul>
+
+
+
+
 
 
 
@@ -30,19 +23,34 @@
             </li>
         </ul>
 
-        <ul class="nav navbar-nav navbar-right">
 
+        <!-- OPCIONES MARCOS-->
+                <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">GESTION DE ASPIRANTES<span class="caret"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">GESTION DE GRUPOS<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="{{route('admin.usuario.index')}}">Mostrar Lista Grupos</a></li>
-                    <li><a href="{{route('admin.usuario.create')}}">Registrar Grupo</a></li>
+                    <li><a href="{{route('admin.usuario.index')}}">Mostrar Lista Usuarios</a></li>
+                    <li><a href="{{route('admin.usuario.create')}}">Registrar nuevo usuario</a></li>
                     <li role="separator" class="divider"></li>
                     <li><a href="#">Cerrar Sesión</a></li>
                 </ul>
             </li>
         </ul>
 
+
+        <!-- OPCIONES MIGUEL-->
+        <ul class="nav navbar-nav navbar-right">
+
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">GESTION DE ASPIRANTES<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="{{route('admin.usuario.index')}}">Mostrar Lista de Aspirantes</a></li>
+                    <li><a href="{{route('admin.usuario.create')}}">Registrar Nuevo Aspirante</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Cerrar Sesión</a></li>
+                </ul>
+            </li>
+        </ul>
 
 
 
