3bbf95b639b4b9636a388b6e09a7dcb8beb395c5
miguel zuñiga <migzunig@alumnos.ubiobio.cl>
2015-12-23 06:54:42 -0300

    part

--- database/migrations/2015_12_23_032030_create_participantes_table.php
+++ database/migrations/2015_12_23_032030_create_participantes_table.php
@@ -19,12 +19,9 @@ class CreateParticipantesTable extends Migration {
 			$table->string('rut_usuario')->unsigned();
 			$table->foreign('rut_usuario')->references('rut_usuario')->on('usuarios')->onDelete('cascade');
 
-<<<<<<< HEAD:database/migrations/2015_12_17_225359_create_participantes_table.php
 
-=======
 			$table->integer('id_grupo')->unsigned();
 			$table->foreign('id_grupo')->references('id_grupo')->on('grupos')->onDelete('cascade');
->>>>>>> origin/master:database/migrations/2015_12_23_032030_create_participantes_table.php
 		});
 	}
 
